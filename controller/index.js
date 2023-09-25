3518cfbcb55807b0049c7ecfdc4808f143e6395e
Nefi Perez <nefipr@gmail.com>
2023-09-25 19:40:37 +0200

    Included new controllers

--- controller/index.js
+++ controller/index.js
@@ -1,11 +1,21 @@
 const mongodb = require('../db/connect');
 
 const getData = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('user').find();
+  const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]); // we just need the first one (the only one)
+    res.status(200).json(lists);
   });
 };
 
-module.exports = { getData };
+const getSingle = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+    .getDb().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]); // we just need the first one (the only one)
+    });    
+};
+
+module.exports = { getData, getSingle };
