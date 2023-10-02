242c5c8c015363dd742d62c72ed0c15744901131
Nefi Perez <nefipr@gmail.com>
2023-10-02 23:43:08 +0200

    Wk03 Assignment Above and beyond 100%

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -22,4 +23,65 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+const createNewContact = async(req, res) => {
+  const newContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const result = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .insertOne(newContact);
+
+  if (result.acknowledged){
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(result.error);
+  }
+
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  
+  if (result.modifiedCount > 0){
+    res.status(204).send();
+  } else {
+    res.status(500).json(result.error);
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .remove({ _id: userId });
+
+  if (result.deletedCount > 0){
+    res.status(200).send();
+  } else {
+    res.status(500).json(result.error);
+  }
+
+};
+
+
+module.exports = { getAll, getSingle, createNewContact, updateContact, deleteContact };
