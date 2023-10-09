8b14f742c94efa15d4fb7b3e38d19b9c7ae73b8e
Nefi Perez <nefipr@gmail.com>
2023-10-09 21:12:46 +0200

    API Documentation

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger'))
 router.use('/contacts', require('./contacts'))
 
 module.exports = router;
+
+
