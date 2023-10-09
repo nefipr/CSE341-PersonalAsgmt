b7f7d0d54d63e5d3ce3e5f15ba050d57f06489d8
Nefi Perez <nefipr@gmail.com>
2023-10-09 21:54:06 +0200

    above and beyond for extra credit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app
       'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', routes);
